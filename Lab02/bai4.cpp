c65b554a65b0f50e89cda7db4e95c9d0136ec45f
baokiinkk <44254528+baokiinkk@users.noreply.github.com>
2018-10-25 20:58:17 +0700

    Update bai4.cpp

--- Lab02/bai4.cpp
+++ Lab02/bai4.cpp
@@ -7,48 +7,50 @@
 #define For(i,a,b)  for( ll i=(a);i<(b);i++)
 #define Fors(i,a,b)  for( ll i=(a);i<=(b);i++)
 using namespace std;
-int UCLN(int a,int b)
+int UCLN(int a, int b)
 {
- while (b!=0)
- {
-   int r=a%b;
-   a=b;
-   b=r;
- }
- return a;
+	while (b != 0)
+	{
+		int r = a%b;
+		a = b;
+		b = r;
+	}
+	return a;
 }
 class PhanSo
 {
-	private:
+private:
 
-		int TuSo, MauSo;
-	public:
-		void in();
-		void out();
+	int TuSo, MauSo;
+public:
+	void in();
+	void out();
 	PhanSo operator+(PhanSo);
 	PhanSo operator-(PhanSo);
 	PhanSo operator*(PhanSo);
 	PhanSo operator/(PhanSo);
-	
+
 };
 void PhanSo::in()
-{x:
-	cout<<"nhap tu so : ";cin>>TuSo;
-	cout<<"\nnhap mau so : ";cin>>MauSo;cout<<endl;
-	if(MauSo==0) {cout<<"khong hop le xin kiem tra lai!\n";goto x;}
+{
+x:
+	cout << "\t\t\tnhap tu so : "; cin >> TuSo;
+	cout << "\t\t\tnhap mau so : "; cin >> MauSo; 
+	if (MauSo == 0) { cout << "khong hop le xin kiem tra lai!\n"; goto x; }
 }
 void PhanSo::out()
 {
-	int s=UCLN(TuSo,MauSo);
-	cout<<"\n sum = ";cout<<TuSo/s<<"/"<<MauSo/s<<endl;
+	int s = UCLN(TuSo, MauSo);
+	cout << "\t\t\t\t   KETQUA = " << TuSo << "/" << MauSo;
+	cout << "\n \t\t\tKETQUA da rut gon = " << TuSo / s << "/" << MauSo / s << endl;
 }
 PhanSo PhanSo::operator+(PhanSo a)
 {
 	PhanSo Tong;
 
-	Tong.TuSo = this ->TuSo * a.MauSo + this ->MauSo * a.TuSo;
+	Tong.TuSo = this->TuSo * a.MauSo + this->MauSo * a.TuSo;
 
-	Tong.MauSo = this ->MauSo * a.MauSo;
+	Tong.MauSo = this->MauSo * a.MauSo;
 
 	return Tong;
 }
@@ -56,9 +58,9 @@ PhanSo PhanSo::operator-(PhanSo a)
 {
 	PhanSo Tong;
 
-	Tong.TuSo = this ->TuSo * a.MauSo - this ->MauSo * a.TuSo;
+	Tong.TuSo = this->TuSo * a.MauSo - this->MauSo * a.TuSo;
 
-	Tong.MauSo = this ->MauSo * a.MauSo;
+	Tong.MauSo = this->MauSo * a.MauSo;
 
 	return Tong;
 }
@@ -66,9 +68,9 @@ PhanSo PhanSo::operator*(PhanSo a)
 {
 	PhanSo Tong;
 
-	Tong.TuSo = this ->TuSo * a.TuSo;
+	Tong.TuSo = this->TuSo * a.TuSo;
 
-	Tong.MauSo = this ->MauSo * a.MauSo;
+	Tong.MauSo = this->MauSo * a.MauSo;
 
 	return Tong;
 }
@@ -76,53 +78,60 @@ PhanSo PhanSo::operator/(PhanSo a)
 {
 	PhanSo Tong;
 
-	Tong.TuSo = this ->TuSo * a.MauSo;
+	Tong.TuSo = this->TuSo * a.MauSo;
 
-	Tong.MauSo = this ->MauSo * a.TuSo;
+	Tong.MauSo = this->MauSo * a.TuSo;
 
 	return Tong;
 }
 int main()
 {
 	string u;
-	PhanSo x,y,s;
-	x:
-	cout<<"phan so A : \n";
+	PhanSo x, y, s;
+x:
+	cout << "\t\t\t========================= PHAN SO A ==========================\n";
 	x.in();
-	cout<<"\nphep toan can tinh :";
-	cin>>u;
-	cout<<"phan so B: \n";
+	cout << "\t\t\t==============================================================\n\n";
+	cout << "\nphep toan can tinh :";
+	cin >> u; cout << endl;
+	cout << "\t\t\t========================= PHAN SO B ==========================\n";
 	y.in();
-	if(u=="+")
+	cout << "\t\t\t==============================================================\n\n\n\n\n";
+	cout << "\t\t-------------------------------------------------------------------------------\n";
+	cout << "\t\t\t===================== KET QUA PHEP TOAN ======================\n";
+	if (u == "+")
 	{
 		string a;
-	s=x+y;s.out();
-	cout<<"Do another (y/n)? ";cin>>a;
-	if(a=="y")goto x;
+		s = x + y; s.out();
+		cout << "\t\t\t==============================================================\n\n";
+		cout << "\t\t\tDo another (y/n)? "; cin >> a;
+		if (a == "y")goto x;
 	}
-	if(u=="-")
+	if (u == "-")
 	{
 		string a;
-	s=x-y;s.out();
-	cout<<"Do another (y/n)? ";cin>>a;
-	if(a=="y")goto x;
+		s = x - y; s.out();
+		cout << "\t\t\t==============================================================\n";
+		cout << "\t\t\tDo another (y/n)? "; cin >> a;
+		if (a == "y")goto x;
 	}
-	if(u=="*")
+	if (u == "*")
 	{
 		string a;
-	s=x*y;s.out();
-	cout<<"Do another (y/n)? ";cin>>a;
-	if(a=="y")goto x;
+		s = x*y; s.out();
+		cout << "\t\t\t==============================================================\n";
+		cout << "\t\t\tDo another (y/n)? "; cin >> a;
+		if (a == "y")goto x;
 	}
-	if(u=="/")
+	if (u == "/")
 	{
 		string a;
-	s=x/y;s.out();
-	cout<<"Do another (y/n)? ";cin>>a;
-	if(a=="y")goto x;
+		s = x / y; s.out();
+		cout << "\t\t\t==============================================================\n";
+		cout << "\t\t\tDo another (y/n)? "; cin >> a;
+		if (a == "y")goto x;
 	}
 
 
 	return 0;
 }
-
